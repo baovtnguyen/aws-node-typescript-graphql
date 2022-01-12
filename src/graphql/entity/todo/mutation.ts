a33e4675fa6f2fff33b24fcc053b4e70a01f18f4
duchhnguyenkms <duc.huu.nguyen.kms@editshare.com>
2022-01-12 21:32:19 +0700

    Implement Todos resolver and update libs

--- src/graphql/entity/todo/mutation.ts
+++ src/graphql/entity/todo/mutation.ts
@@ -1,7 +1,116 @@
+import { nanoid } from 'nanoid';
 import type { Todo } from '../../../interfaces/todo';
+import { generateTodoSortKey } from '../../../libs/helpers'
 
 export default {
   Mutation: {
-    createTodo: () => 'Hello, World!',
+    createTodo: async (parent, args, { dynamodb }, info): Promise<Todo> => {
+      try {
+        const { userID, content } = args;
+
+        const newTodo = {
+          pk: process.env.TODO_APP_PK,
+          sk: generateTodoSortKey(userID, nanoid()),
+          content: content,
+          isCompleted: false,
+        };
+
+        const params = {
+          TableName: process.env.DYNAMODB_TABLE_NAME,
+          Item: newTodo,
+          ConditionExpression:
+            'attribute_not_exists(pk) AND attribute_not_exists(sk)',
+        };
+
+        await dynamodb.put(params).promise();
+
+        const [ _, returnUserID, returnTodoID ] = newTodo.sk.split('::');
+
+        return {
+          userID: returnUserID,
+          todoID: returnTodoID,
+          content: newTodo.content,
+          isCompleted: newTodo.isCompleted,
+        };
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+    updateTodo: async (parent, args, { dynamodb }, info): Promise<Todo> => {
+      try {
+        const { userID, todoID, content, isCompleted } = args.todo;
+        const updateAttributes = [];
+        const expressionAttributeNames = {};
+        const expressionAttributeValues = {};
+
+        if (content !== undefined) {
+          updateAttributes.push('#content = :content');
+          expressionAttributeNames['#content'] = 'content';
+          expressionAttributeValues[':content'] = content;
+        }
+        if (isCompleted !== undefined) {
+          updateAttributes.push('#isCompleted = :isCompleted');
+          expressionAttributeNames['#isCompleted'] = 'isCompleted';
+          expressionAttributeValues[':isCompleted'] = isCompleted;
+        }
+
+        const updateExpression = 'SET ' + updateAttributes.join(',');
+
+        const params = {
+          TableName: process.env.DYNAMODB_TABLE_NAME,
+          Key: {
+            pk: process.env.TODO_APP_PK,
+            sk: generateTodoSortKey(userID, todoID),
+          },
+          ConditionExpression: 'attribute_exists(pk) AND attribute_exists(sk)',
+          UpdateExpression: updateExpression,
+          ExpressionAttributeNames: expressionAttributeNames,
+          ExpressionAttributeValues: expressionAttributeValues,
+          ReturnValues: 'ALL_NEW',
+        };
+
+        const res = await dynamodb.update(params).promise();
+        const returnTodo = res.Attributes;
+        const [ _, returnUserID, returnTodoID ] = returnTodo.sk.split('::');
+
+        return {
+          userID: returnUserID,
+          todoID: returnTodoID,
+          content: returnTodo.content,
+          isCompleted: returnTodo.isCompleted,
+        };
+      } catch (err) {
+        return err;
+      }
+    },
+    deleteTodo: async (parent, args, { dynamodb }, info): Promise<Todo> => {
+      try {
+        const { userID, todoID } = args;
+
+        const params = {
+          TableName: process.env.DYNAMODB_TABLE_NAME,
+          Key: {
+            pk: process.env.TODO_APP_PK,
+            sk: generateTodoSortKey(userID, todoID),
+          },
+          ConditionExpression: 'attribute_exists(pk) AND attribute_exists(sk)',
+          ReturnValues: 'ALL_OLD',
+        };
+
+        const res = await dynamodb.delete(params).promise();
+
+        const returnTodo = res.Attributes;
+        const [ _, returnUserID, returnTodoID ] = returnTodo.sk.split('::');
+
+        return {
+          userID: returnUserID,
+          todoID: returnTodoID,
+          content: returnTodo.content,
+          isCompleted: returnTodo.isCompleted,
+        };
+      } catch (err) {
+        return err;
+      }
+    },
   },
 };
--- src/graphql/entity/todo/query.ts
+++ src/graphql/entity/todo/query.ts
@@ -1,23 +1,74 @@
 import type { Todo } from '../../../interfaces/todo';
+import { generateTodoSortKey } from '../../../libs/helpers';
 
 export default {
   Query: {
-    hello: () => {
-      return 'Hello, World!';
-    },
-
-    getTodos: async (parent, args, { dynamodb }, info) => {
-      const params = {
-        TableName: process.env.DYNAMODB_TABLE_NAME,
-        KeyConditionExpression: `pk = :pk`,
-        ExpressionAttributeValues: {
-          ':pk': process.env.TODO_APP_PK,
-        },
-      };
-
+    getTodos: async (parent, args, { dynamodb }, info): Promise<Todo[]> => {
       try {
+        const params = {
+          TableName: process.env.DYNAMODB_TABLE_NAME,
+          KeyConditionExpression: `#pk = :pk and begins_with(#sk, :sk)`,
+          ExpressionAttributeNames:{
+            "#pk": "pk",
+            "#sk": 'sk'
+          },
+          ExpressionAttributeValues: {
+            ':pk': process.env.TODO_APP_PK,
+            ':sk': generateTodoSortKey()
+          },
+        };
         const result = await dynamodb.query(params).promise();
-        return result.Items;
+
+        const returnTodos = [];
+        for (const todo of result.Items) {
+          const [ _, returnUserID, returnTodoID ] = todo.sk.split('::');
+
+          returnTodos.push({
+            userID: returnUserID,
+            todoID: returnTodoID,
+            content: todo.content,
+            isCompleted: todo.isCompleted,
+          });
+        }
+
+        return returnTodos;
+
+      } catch (err) {
+        throw err;
+      }
+    },
+    getTodosOfUser: async (parent, args, { dynamodb }, info): Promise<Todo[]> => {
+      try {
+        const { userID } = args;
+        const params = {
+          TableName: process.env.DYNAMODB_TABLE_NAME,
+          KeyConditionExpression: `#pk = :pk and begins_with(#sk, :sk)`,
+          ExpressionAttributeNames:{
+            "#pk": "pk",
+            "#sk": 'sk'
+          },
+          ExpressionAttributeValues: {
+            ':pk': process.env.TODO_APP_PK,
+            ':sk': generateTodoSortKey(userID)
+          },
+        };
+        const result = await dynamodb.query(params).promise();
+        const returnTodo = result.Items;
+
+        const returnTodos = [];
+        for (const todo of result.Items) {
+          const [ _, returnUserID, returnTodoID ] = todo.sk.split('::');
+
+          returnTodos.push({
+            userID: returnUserID,
+            todoID: returnTodoID,
+            content: todo.content,
+            isCompleted: todo.isCompleted,
+          });
+        }
+
+        return returnTodos;
+
       } catch (err) {
         throw err;
       }
